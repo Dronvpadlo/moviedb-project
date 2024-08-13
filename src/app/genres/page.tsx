1deb7199b6323c67854d5e910b97c049293e2e59
User <doronyuk714@mail.com>
2024-08-13 21:06:45 +0300

    get films & get genres

--- src/app/genres/page.tsx
+++ src/app/genres/page.tsx
@@ -1,6 +1,5 @@
 import React from 'react';
 import { movieService } from "@/services/api.service";
-import {IGenre, IGenres} from "@/models/IGenres";
 
 const Page = async () => {
     const allGenres = await movieService.allGenres();
